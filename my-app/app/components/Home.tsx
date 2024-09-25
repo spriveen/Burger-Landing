66e90b08533995d2829abad85153856a6f74ae02
spriveen <rpeanuka@gmail.com>
2024-09-25 05:03:51 -0700

    Update Delivery.tsx

--- my-app/app/components/Home.tsx
+++ my-app/app/components/Home.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Hero from './Hero/Hero';
 import Feature from './Features/Feature';
 import PopularBurger from './PopularBurger/PopularBurger';
-
+import Delivery from './Delivery/Delivery';
 
 const Home = () => {
   return (
-  <div className='overflow-hidden'>
-    <Hero />
-    <Feature />
-    <PopularBurger />
-    
-  </div>
-  ) 
+    <div className='overflow-hidden'>
+      <Hero />
+      <Feature />
+      <PopularBurger />
+      <Delivery />
+    </div>
+  );
 };
 
-export default Home
+export default Home;
